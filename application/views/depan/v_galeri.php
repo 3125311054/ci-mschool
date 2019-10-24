93efb273df6077ef75bcec7c819cfb2e72fc3329
muhammad fuad <56967411+muhammadfuad5@users.noreply.github.com>
2019-10-25 06:03:44 +0700

    update v_galeri.php

--- application/views/depan/v_galeri.php
+++ application/views/depan/v_galeri.php
@@ -96,6 +96,9 @@
                               <li class="nav-item">
                                 <a class="nav-link" href="<?php echo site_url('contact');?>">Contact</a>
                               </li>
+                             <li class="nav-item">
+                                <a class="nav-link" href="<?php echo site_url('login');?>">Login</a>
+                              </li>
                         </ul>
                   </div>
                 </nav>
